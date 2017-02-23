a9dac9a33f0698de61a6cdf954b4617297233537
Yuta Hongo <yutago@gmail.com>
2017-02-23 11:55:54 +0900

    Set Chrome User-Agent

--- idol_collector/settings.py
+++ idol_collector/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'idol_collector.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'idol_collector (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
